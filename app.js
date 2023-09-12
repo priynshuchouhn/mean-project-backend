230134e2a653a109459bdadc64b5d925f7ccc892
Priyanshu Chouhan <priyanschouhan123@gmail.com>
2023-09-12 22:23:41 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+
+app.use((req,res,next)=>{
+    console.log("first middleware");
+    next();
+})
+
+app.use((req,res,next)=>{
+    res.send('Hello from Nodemon');
+})
+
+module.exports = app;
