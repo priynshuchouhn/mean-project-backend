2ecac25abb693d13d6e9f0e6f7a13879769afb0b
Priyanshu Chouhan <priyanschouhan123@gmail.com>
2023-09-13 08:39:39 +0530

    first API created

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const express = require('express');
 const app = express();
 
 
-app.use((req,res,next)=>{
-    console.log("first middleware");
-    next();
+app.use('/api/posts',(req,res,next)=>{
+    const posts = [
+        {id:1, title: 'First post', content:'Content of first post!'},
+        {id:2, title: 'second post', content:'Content of second post!'},
+        {id:3, title: 'Third post', content:'Content of third post!'},
+    ];
+   res.status(200).json({
+    message : 'Post fetched successfully',
+    data: posts,
+    success: true
+   })
 })
-
-app.use((req,res,next)=>{
-    res.send('Hello from Nodemon');
-})
-
 module.exports = app;
