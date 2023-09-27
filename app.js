182c340d71ea72104ca617067733afc943821d7c
Priyanshu Chouhan <priyanschouhan123@gmail.com>
2023-09-27 21:43:48 +0530

    minor fixes

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ app.use((req,res,next)=>{
     next();
 })
 
-app.post('/api/posts', (req,res,next)=>{
-    const post = req.body;
+app.post('/api/posts',(req,res,next)=>{
+const post = req.body;
 console.log(post);
 res.status(200).json({
     message : 'Post Added successfully',
@@ -21,7 +21,7 @@ res.status(200).json({
 })
 
 
-app.use('/api/posts',(req,res,next)=>{
+app.get('/api/posts',(req,res,next)=>{
     const posts = [
         {id:1, title: 'First post', content:'Content of first post!'},
         {id:2, title: 'second post', content:'Content of second post!'},
