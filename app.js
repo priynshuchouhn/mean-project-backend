4334eddc959cf7929c2c7628277c1ab543e0e060
Priyanshu Chouhan <priyanschouhan123@gmail.com>
2023-10-03 22:48:52 +0530

    minor update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express  from "express";
 
 const app = express();
 
-app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use((req,res,next)=>{
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
